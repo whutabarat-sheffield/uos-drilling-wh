c031fbfca52f4313b5e92a95f7c504c1764451f2
whutabarat-sheffield <38495025+whutabarat-sheffield@users.noreply.github.com>
2024-12-16 16:47:05 +0000

    New setup.py to include cuda

--- abyss/setup.py
+++ abyss/setup.py
@@ -1,4 +1,7 @@
+import os
 import setuptools
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -13,6 +16,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 # }
 
 # uncomment if on public internet
+
+LOCAL_DEPENDENCIES = {
+    # 'transformers': 'transformers-4.48.0.dev0.tar.gz', # gives wrong result
+    'transformers': 'transformers-4.41.0.dev0.tar.gz',
+    'tsfm-public': 'tsfm_public-0.2.17.tar.gz',
+    'accelerate': 'accelerate-1.2.1.tar.gz',
+}
+
 DEPENDENCIES = {
     'transformers': 'https://github.com/whutabarat-sheffield/transformers-wh.git',
     'tsfm-public': 'https://github.com/ibm-granite/granite-tsfm.git',
@@ -20,12 +31,17 @@ DEPENDENCIES = {
 }
 
 def get_dependency(package, location):
-    if location == 'install':
+    if location == 'git':
         return f'{package} @ git+{DEPENDENCIES[package]}'
     elif location == 'link':
         return f'{DEPENDENCIES[package]}#egg={package}'
+    elif location == 'local':
+        local_dep = os.path.join(CURRENT_DIR, '..', 'deps', f'{LOCAL_DEPENDENCIES[package]}')
+        return f'{package} @ file:///{local_dep}'
     else:
         raise ValueError(f'Unknown location: {location}')
+
+
 
 setuptools.setup(
     name="abyss",
@@ -54,24 +70,23 @@ setuptools.setup(
         'h5py',
         'nptdms',
         'scaleogram',
-        'torch',
+        # 'nvidia_cuda_runtime_cu11',
+        # 'pytorch-cuda==11.8',
+        # 'torch@https://download.pytorch.org/whl/cu118/torch-2.3.1%2Bcu118-cp310-cp310-win_amd64.whl',
+        'torch==2.3.1+cu118',
         'torchvision',
         'torchaudio',
         'numpy',
         'pandas',
         'matplotlib',
-        # 'pytorch-cuda',
         'tensorboardX',
         'ipykernel',
         'jupyter',
-        get_dependency('transformers', location='install'),
-        get_dependency('tsfm-public', location='install'),
-        get_dependency('accelerate', location='install'),
-    ],
-    dependency_links=[
-        get_dependency('transformers', location='link'),
-        get_dependency('tsfm-public', location='link'),
-        get_dependency('accelerate', location='link'),
+        'paho-mqtt',
+        'pyyaml',
+        get_dependency('transformers', location='local'),
+        get_dependency('tsfm-public', location='local'),
+        get_dependency('accelerate', location='local'),
     ],
     entry_points={
         'console_scripts': [
@@ -79,4 +94,59 @@ setuptools.setup(
         ],
     },
     include_package_data=True,
+
+
+# setuptools.setup(
+#     name="abyss",
+#     version="2.0.0",
+#     author="David Miller",
+#     author_email="d.b.miller@sheffield.ac.uk",
+#     description="Electric drill data processing",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.airbus.corp/Airbus/uos-drilling",
+#     project_urls={
+#         "Bug Tracker": "https://github.airbus.corp/Airbus/uos-drilling/issues",
+#     },
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "Operating System :: OS Independent",
+#     ],
+#     package_dir={"": "src"},
+#     packages=setuptools.find_packages(where="src"),
+#     python_requires=">=3.9",
+#     install_requires=[
+#         'numpy',
+#         'pandas',
+#         'matplotlib',
+#         'openpyxl',
+#         'h5py',
+#         'nptdms',
+#         'scaleogram',
+#         'torch',
+#         'torchvision',
+#         'torchaudio',
+#         'numpy',
+#         'pandas',
+#         'matplotlib',
+#         # 'pytorch-cuda',
+#         'tensorboardX',
+#         'ipykernel',
+#         'jupyter',
+#         'paho-mqtt'
+#         get_dependency('transformers', location='git'),
+#         get_dependency('tsfm-public', location='git'),
+#         get_dependency('accelerate', location='git'),
+#     ],
+#     dependency_links=[
+#         get_dependency('transformers', location='link'),
+#         get_dependency('tsfm-public', location='link'),
+#         get_dependency('accelerate', location='link'),
+#     ],
+#     entry_points={
+#         'console_scripts': [
+#             'depth-est = abyss:run.uos_depth_estimation',
+#         ],
+#     },
+#     include_package_data=True,
 )
