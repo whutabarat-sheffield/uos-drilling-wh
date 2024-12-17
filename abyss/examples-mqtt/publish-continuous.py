ea0cf79316e696ca1482062500da79d163be8f66
whutabarat-sheffield <38495025+whutabarat-sheffield@users.noreply.github.com>
2024-12-17 12:29:55 +0000

    Got the publisher working for testing

--- abyss/examples-mqtt/publish-continuous.py
+++ abyss/examples-mqtt/publish-continuous.py
@@ -1,11 +1,13 @@
 import paho.mqtt.client as mqtt
 import time
 import json
-import sysconfig
-from pathlib import Path
+# import sysconfig
+# from pathlib import Path
 import os
+# import yaml
 
-TOPIC = "test/topic"
+TOPIC_RESULT = "ResultManagement/Result"
+TOPIC_TRACE = "ResultManagement/Trace"
 
 client = mqtt.Client()
 client.connect("localhost", 1883)
@@ -17,23 +19,23 @@ client.connect("localhost", 1883)
 
 print(os.getcwd())
 with open (f'{os.getcwd()}\\data\\Result.json') as f:
-    d0 = f.read()
+    d_result = f.read()
 with open (f'{os.getcwd()}\\data\\Trace.json') as f:
-    d1 = f.read()
+    d_trace = f.read()
 
-def publish(payload, timestamp):
+def publish(topic, payload, timestamp):
     # print(payload)
     d = json.loads(payload)
     # print(d['Messages'])
     d['Messages']['Timestamp'] = timestamp
     # client.publish("test/topic", json.dumps(payload))
-    client.publish("TOPIC", json.dumps(d))
+    client.publish(topic, json.dumps(d))
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
-    print(f"[{current_time}]: Published drilling MQTT data on {TOPIC} '{timestamp}'")
+    print(f"[{current_time}]: Publish data on {topic} '{timestamp}'")
 
 while True:
     timestamp = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.localtime())
-    publish(d0, timestamp)
-    publish(d1, timestamp)    
+    publish(TOPIC_RESULT, d_result, timestamp)
+    publish(TOPIC_TRACE, d_trace, timestamp)    
     time.sleep(5)
