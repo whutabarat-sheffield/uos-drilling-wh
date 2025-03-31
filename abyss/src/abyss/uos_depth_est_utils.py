900e0171f4d6d3973cca325216bf0d33d122c584
whutabarat-sheffield <38495025+whutabarat-sheffield@users.noreply.github.com>
2025-03-31 10:29:59 +0100

    Enhance logging and update data extraction in MQTT to DataFrame conversion

--- abyss/src/abyss/uos_depth_est_utils.py
+++ abyss/src/abyss/uos_depth_est_utils.py
@@ -36,9 +36,14 @@ def convert_mqtt_to_df(result_msg=None, trace_msg=None, conf=None):
     - The paths in the configuration should be provided as tuples representing the hierarchical keys.
     """
     
+
+
     def reduce_dict(data_dict, search_key):
         # Using reduce function to filter dictionary values based on search_key
         # from https://www.geeksforgeeks.org/python-substring-key-match-in-dictionary/
+        logging.info("Reducing dictionary")
+        logging.debug(f"Dict: {data_dict}\n\nSearch_key: {search_key}")
+    
         values = reduce(
             # lambda function that takes in two arguments, an accumulator list and a key
             lambda acc, key: acc + [data_dict[key]] if search_key in key else acc,
@@ -48,40 +53,52 @@ def convert_mqtt_to_df(result_msg=None, trace_msg=None, conf=None):
             []
         )
         return values[0]['Value']
+    
 
     def parse_result(data, conf):
         # Default key paths for RESULT structure
         fore = ['Messages', 'Payload']
         
         data = data[fore[0]][fore[1]]   
+        logging.info(conf['mqtt']['data_ids']['torque_empty_vals'])
 
-        torque_empty_vals = reduce_dict(data, conf['torque_empty_vals'])
-        logging.debug(f"Torque empty values: {torque_empty_vals}")
-        thrust_empty_vals = reduce_dict(data, conf['thrust_empty_vals'])
-        logging.debug(f"Thrust empty values: {thrust_empty_vals}")
-        step_vals = reduce_dict(data, conf['step_vals'])
-        logging.debug(f"Step values: {step_vals}")
-        hole_id = reduce_dict(data, conf['machine_id'])
-        logging.debug(f"Hole ID: {hole_id}")
+        torque_empty_vals = reduce_dict(data, conf['mqtt']['data_ids']['torque_empty_vals'])
+        logging.info(f"Torque empty values: {torque_empty_vals}")
+        thrust_empty_vals = reduce_dict(data, conf['mqtt']['data_ids']['thrust_empty_vals'])
+        logging.info(f"Thrust empty values: {thrust_empty_vals}")
+        step_vals = reduce_dict(data, conf['mqtt']['data_ids']['step_vals'])
+        logging.info(f"Step values: {step_vals}")
+        # hole_id = reduce_dict(data, conf['mqtt']['data_ids']['machine_id']) + '_' + reduce_dict(data, conf['mqtt']['data_ids']['result_id'])
+        hole_id = reduce_dict(data, conf['mqtt']['data_ids']['machine_id'])# TODO rethink whether this is correct
+        hole_id = [str(hole_id)] * len(step_vals)
+        logging.info(f"Hole ID: {hole_id}")
+        local = reduce_dict(data, conf['mqtt']['data_ids']['result_id'])# TODO rethink whether this is correct
+        local = [str(local)] * len(step_vals)
+        logging.info(f"Local count: {local}")
 
         # Create a DataFrame
+        logging.info("Creating DataFrame")
         try:
             df = pd.DataFrame({
                 'Step (nb)': step_vals,
                 'I Torque Empty (A)': torque_empty_vals,
                 'I Thrust Empty (A)': thrust_empty_vals,
-                'HOLE_ID': [str(hole_id)] * len(step_vals),
-                'PREDRILLED': [1] * len(step_vals)
+                # 'HOLE_ID': [str(hole_id)] * len(step_vals),
+                # 'local': [str(local)] * len(step_vals),
+                'HOLE_ID': hole_id,
+                'local': local,
+                'PREDRILLED': [1] * len(step_vals),
             })
         except:
             df = pd.DataFrame({
                 'Step (nb)': step_vals,
                 'I Torque Empty (A)': torque_empty_vals,
                 'I Thrust Empty (A)': thrust_empty_vals,
-                'HOLE_ID': [str(hole_id)],
+                'HOLE_ID': [str(hole_id)], 
+                'local': [str(local)],  
                 'PREDRILLED': [1]
             })
-
+        logging.info(f"DataFrame: {df}")
         return df
 
     def parse_trace(data, conf):
@@ -94,14 +111,15 @@ def convert_mqtt_to_df(result_msg=None, trace_msg=None, conf=None):
         #     key_path = fore + [key]
         #     return reduce(dict.get, key_path, data)
 
-        position = reduce_dict(data, conf['position'])
+        position = reduce_dict(data, conf['mqtt']['data_ids']['position'])
         logging.debug(f"Position: {position}")
-        torque = reduce_dict(data, conf['torque'])
+        torque = reduce_dict(data, conf['mqtt']['data_ids']['torque'])
         logging.debug(f"Torque: {torque}")
-        thrust = reduce_dict(data, conf['thrust'])
+        thrust = reduce_dict(data, conf['mqtt']['data_ids']['thrust'])
         logging.debug(f"Thrust: {thrust}")
-        step = reduce_dict(data, conf['step'])
+        step = reduce_dict(data, conf['mqtt']['data_ids']['step'])
         logging.debug(f"Step: {step}")
+        # hole_id = reduce_dict(data, conf['mqtt']['data_ids']['trace_result_id'])
 
         # Create a DataFrame
         df = pd.DataFrame({
@@ -109,19 +127,28 @@ def convert_mqtt_to_df(result_msg=None, trace_msg=None, conf=None):
             'Position (mm)': position,
             'I Torque (A)': torque,
             'I Thrust (A)': thrust,
+            # 'HOLE_ID': [str(hole_id)],
         })
 
         # Process columns
         df['Step (nb)'] = df['Step (nb)'].astype('int32')
         df['Position (mm)'] = -df['Position (mm)']  # Invert position
 
+        logging.info(f"Trace DataFrame: {df.tail()}")
         return df
 
     result_df = None
     trace_df = None
 
+    logging.debug("Starting conversion of MQTT messages to DataFrame")
+    logging.debug(f"Result message: {result_msg}\n\n")
+    logging.debug(f"Trace message: {trace_msg}\n\n")
+    logging.debug(f"Configuration: {conf}\n\n")
+
     if result_msg:
         result_data = json.loads(result_msg)
+        result_df = parse_result(result_data, conf)
+
         try:
             result_df = parse_result(result_data, conf)
         except Exception as e:
@@ -138,7 +165,8 @@ def convert_mqtt_to_df(result_msg=None, trace_msg=None, conf=None):
     if result_df is not None and trace_df is not None:
         try:
             combined_df = pd.merge(result_df, trace_df, on='Step (nb)', how='outer')
-            logging.debug(str(combined_df.dtypes))
+            logging.info(str(combined_df.dtypes))
+            logging.info(f"Combined DataFrame: {combined_df.tail()}")
             return combined_df
         except Exception as e:
             logging.critical("Error merging RESULT and TRACE DataFrames: %s", str(e))
